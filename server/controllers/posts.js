69c38b244153e09f426d90732aef98a39e88c41c
YubaNeupane <neopaneyb2@gmail.com>
2021-11-19 20:56:04 -0500

    added to like post in the server

--- server/controllers/posts.js
+++ server/controllers/posts.js
@@ -51,3 +51,22 @@ export const deletePost = async (req, res) => {
   res.json({ message: "Post deleted successfully." });
 };
 
+export const likePost = async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id))
+    return res.status(404).send(`No post with id: ${id}`);
+
+  const post = await PostMessage.findById(id);
+
+  const updatePost = await PostMessage.findById(
+    id,
+    {
+      likeCount: post.likeCount + 1,
+    },
+    { new: true }
+  );
+
+  res.json(updatePost);
+};
+
--- server/routes/posts.js
+++ server/routes/posts.js
@@ -9,11 +9,13 @@ import {
   createPost,
   updatePost,
   deletePost,
+  likePost,
 } from "../controllers/posts.js";
 
 router.get("/", getPost);
 router.post("/", createPost);
 router.patch("/:id", updatePost);
 router.delete("/:id", deletePost);
+router.patch("/:id/likePost", likePost);
 
 export default router
