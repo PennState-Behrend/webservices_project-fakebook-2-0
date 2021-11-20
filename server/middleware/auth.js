da0be21375ab1b68839a6d3157bf1814b569a23d
YubaNeupane <neopaneyb2@gmail.com>
2021-11-19 22:29:34 -0500

    fixed bug with liking post

--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -1,13 +1,15 @@
 import jwt from "jsonwebtoken";
 
 const auth = async (req, res, next) => {
+  
   try {
+    console.log(req.headers);
     const token = req.headers.authorization.split(" ")[1];
 
     let decodedData;
 
     if (token) {
-      decodedData = jwt.verify(token, secret);
+      decodedData = jwt.verify(token, "test");
 
       req.userId = decodedData.id;
     }
